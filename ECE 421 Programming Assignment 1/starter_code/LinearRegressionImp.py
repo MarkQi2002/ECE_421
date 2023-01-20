f5db324d0e297350c16d85be3c48257d8d3b86a1
Mark Qi <mark.qi@mail.utoronto.ca>
2023-01-19 21:50:32 -0500

    Adjusted Directory For PA1

--- ECE 421 Programming Assignment 1/starter_code/LinearRegressionImp.py
+++ ECE 421 Programming Assignment 1/starter_code/LinearRegressionImp.py
@@ -6,63 +6,16 @@ from sklearn.metrics import mean_squared_error
 
 
 def fit_LinRegr(X_train, y_train):
-    # Input:
-    # X_train represents the matrix of input features that belongs to R (N By d)
-    # N is the total number of training samples
-    # d is the dimension of each input feature vector
+    #Add implementation here
 
-    # y_train is an N dimensional vector where the ith component represents 
-    # the output observed in the training set for the ith row in X_train matrix
-    # which corresponds to the ith input feature (+1 or -1)
+def mse(X_train,y_train,w):
+    #Add implementation here
 
-    # Output: 
-    # Vector w that represents the coefficients of the line computed by the pocket algorithm 
-    # that best separates the two classes of training data points
-    # Vector w has dimensions d + 1
-
-    # Variable Declaration And Intialize Weight Vector
-    w = np.zeros(X_train.shape[1] + 1)
-
-    # Add Additional Column To The Front Of X_train
-    one_column = np.ones((X_train.shape[0], 1))
-    X_train = np.hstack((one_column, X_train))
-
-    # Compute Pseudo-Inversion
-    X_train_transposed = np.transpose(X_train)
-    pseudo_inverse = np.matmul(np.linalg.pinv(np.matmul(X_train_transposed, X_train)), X_train_transposed)
-
-    # Return Weight Vector
-    return np.matmul(pseudo_inverse, y_train)
-
-
-def mse(X_train, y_train, w):
-    # Variable Declaration
-    result = 0
-
-    # Add Additional Column To The Front Of X_train
-    one_column = np.ones((X_train.shape[0], 1))
-    X_train = np.hstack((one_column, X_train))
-
-    # Output Mean Squared Error
-    for i in range(X_train.shape[0]):
-        result += pow(pred(X_train[0], w) - y_train[i], 2)
-
-    # Return Mean Squared Error
-    return result
-
-def pred(X_train, w):
-    # Predicted Value Based On Weight Vector
-    return np.dot(X_train, w)
+def pred(X_train,w):
+    #Add implementation here
 
 def test_SciKit(X_train, X_test, Y_train, Y_test):
-    # Training Linear Regression Model From scikit-learn
-    LR = linear_model.LinearRegression().fit(X_train, Y_train)
-
-    # Calculate Mean Squared Error From scikit-learn
-    lr_err = mean_squared_error(LR.predict(X_test), Y_test)
-
-    # Return Final Mean Squared Error Rate
-    return lr_err
+    #Add implementation here
 
 def subtestFn():
     # This function tests if your solution is robust against singular matrix
@@ -79,13 +32,13 @@ def subtestFn():
       print ("ERROR")
 
 def testFn_Part2():
-    X_train, y_train = load_diabetes(return_X_y = True)
-    X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.2)
+    X_train, y_train = load_diabetes(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(X_train,y_train,test_size=0.2)
     
-    w = fit_LinRegr(X_train, y_train)
+    w=fit_LinRegr(X_train, y_train)
     
     #Testing Part 2a
-    e = mse(X_test, y_test, w)
+    e=mse(X_test,y_test,w)
     
     #Testing Part 2b
     scikit=test_SciKit(X_train, X_test, y_train, y_test)
@@ -98,7 +51,3 @@ subtestFn()
 
 print ('------------------testFn_Part2-------------------')
 testFn_Part2()
-
-# Mean Squared Error Computed From Our Own Linear Regression Model Were 910,406
-# Mean Squared Error Computed From scikit-learn Library Were 2,867
-# The Difference Were A Lot, Our Model Didn't Perform Too Well
